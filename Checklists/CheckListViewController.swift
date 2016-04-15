08088223f34a700152fe02cd081137168ddf20f1
Roman Ustuantsev <liberrom.hse@gmail.com>
2016-04-15 13:46:44 +0300

    added tableView

---
+++ Checklists/CheckListViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Roman Ustiantcev on 15/04/16.
+//  Copyright © 2016 Roman Ustiantcev. All rights reserved.
+//
+
+import UIKit
+
+class CheckListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

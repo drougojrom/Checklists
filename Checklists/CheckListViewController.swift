e72ed6de4774d83b6bc2c90f8d2c72698b748ce2
Roman Ustuantsev <liberrom.hse@gmail.com>
2016-04-15 15:26:50 +0300

    added tableView

--- Checklists/CheckListViewController.swift
+++ Checklists/CheckListViewController.swift
@@ -10,6 +10,50 @@ import UIKit
 
 class CheckListViewController: UITableViewController {
 
+    // MARK: var and let
+    
+    var row0item: ChecklistItem
+    var row1item: ChecklistItem
+    var row2item: ChecklistItem
+    var row3item: ChecklistItem
+    var row4item: ChecklistItem
+    
+    required init?(coder aDecoder: NSCoder){
+        
+        
+        
+        super.init(coder: aDecoder)
+    }
+    
+    // MARK : func's
+    
+    func configureCheckmarkForCell(cell: UITableViewCell, indexPath: NSIndexPath) {
+        
+        var isChecked = false
+        
+        if indexPath.row == 0 {
+            isChecked = row0item.checked
+        } else if indexPath.row == 1 {
+            isChecked = row1item.checked
+        } else if indexPath.row == 2 {
+            isChecked = row2item.checked
+        } else if indexPath.row == 3 {
+            isChecked = row3item.checked
+        } else if indexPath.row == 4 {
+            isChecked = row4item.checked
+        }
+        
+        if isChecked {
+            cell.accessoryType = .Checkmark
+        } else {
+            cell.accessoryType = .None
+        }
+        
+    }
+    
+    
+    // MARK : override's
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +64,59 @@ class CheckListViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("ChecklistItem", forIndexPath: indexPath)
+        let label = cell.viewWithTag(1000) as! UILabel
+        
+        if indexPath.row == 0 {
+            label.text = row0item.text
+        } else if indexPath.row == 1 {
+            label.text = row1item.text
+        } else if indexPath.row == 2 {
+            label.text = row2item.text
+        } else if indexPath.row == 3 {
+            label.text = row3item.text
+        } else if indexPath.row == 4 {
+            label.text = row3item.text
+        }
+        
+        configureCheckmarkForCell(cell, indexPath: indexPath)
+        return cell
+    }
+    
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        if let cell = tableView.cellForRowAtIndexPath(indexPath) {
+            
+            if indexPath.row == 0 {
+                row0item.checked = !row0item.checked
+
+            } else if indexPath.row == 1 {
+                row1item.checked = !row1item.checked
+
+            } else if indexPath.row == 2 {
+                row2item.checked = !row2item.checked
+
+            } else if indexPath.row == 3 {
+                row3item.checked = !row3item.checked
+
+            } else if indexPath.row == 4 {
+                row4item.checked = !row4item.checked
+            }
+            
+            configureCheckmarkForCell(cell, indexPath: indexPath)
+        }
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+    
+    
 
 }
 
---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,16 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Roman Ustiantcev on 15/04/16.
+//  Copyright © 2016 Roman Ustiantcev. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+    
+    var text = ""
+    var checked = false
+    
+}

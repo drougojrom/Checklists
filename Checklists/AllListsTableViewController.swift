6a389b5287b54a8fa8ac83c8eab7fe1ef0150b64
Roman Ustuantsev <liberrom.hse@gmail.com>
2016-04-21 15:37:45 +0300

    added new tableView for lists of detailed view

---
+++ Checklists/AllListsTableViewController.swift
@@ -0,0 +1,61 @@
+//
+//  AllListsTableViewController.swift
+//  Checklists
+//
+//  Created by Roman Ustiantcev on 21/04/16.
+//  Copyright © 2016 Roman Ustiantcev. All rights reserved.
+//
+
+import UIKit
+
+class AllListsTableViewController: UITableViewController {
+    
+    
+    func cellForTableView(tableView: UITableView) -> UITableViewCell {
+        
+        let cellIdentifier = "Cell"
+        if let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) {
+            return cell
+        } else {
+            return UITableViewCell(style: .Default, reuseIdentifier: cellIdentifier)
+        }
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 3
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = cellForTableView(tableView)
+        cell.textLabel!.text = "List \(indexPath.row)"
+
+
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        performSegueWithIdentifier("ShowChecklist", sender: nil)
+        
+    }
+    
+}

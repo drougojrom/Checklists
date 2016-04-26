6cd7eebfbc881d1261b874678977c9b004460155
Roman Ustuantsev <liberrom.hse@gmail.com>
2016-04-26 12:12:43 +0300

    v0.7 added

---
+++ Checklists/ListDetailViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ListDetailViewController.swift
+//  Checklists
+//
+//  Created by Roman Ustiantcev on 21/04/16.
+//  Copyright © 2016 Roman Ustiantcev. All rights reserved.
+//
+
+import UIKit
+
+protocol ListDetailViewControllerDelegate: class {
+    func listDetailViewControllerDidCancel(controller: ListDetailViewController)
+    func listDetailViewController(controller: ListDetailViewController, didFinishAddingChecklist checklist: Checklist)
+    func listDetailViewController(controller: ListDetailViewController, didFinishEditingChecklist checklist: Checklist)
+}
+
+class ListDetailViewController: UITableViewController, UITextFieldDelegate {
+
+    // MARK : - instances
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    
+    weak var delegate: ListDetailViewControllerDelegate?
+    
+    var checklistToEdit: Checklist?
+    
+    // MARK : - IBActions
+    
+    @IBAction func done(){
+        if let checklist = checklistToEdit {
+            checklist.name = textField.text!
+            delegate?.listDetailViewController(self, didFinishEditingChecklist: checklist)
+        } else {
+            let checklist = Checklist(name: textField.text!)
+            delegate?.listDetailViewController(self, didFinishAddingChecklist: checklist)
+        }
+    }
+    
+    @IBAction func cancel(){
+        delegate?.listDetailViewControllerDidCancel(self)
+    }
+    
+    // MARK : - func's
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        let oldText: NSString = textField.text!
+        let newText: NSString = oldText.stringByReplacingCharactersInRange(range, withString: string)
+        doneBarButton.enabled = (newText.length > 0)
+        return true
+    }
+    
+    // MARK : - override
+    
+    override func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
+        return nil
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let checklist = checklistToEdit {
+            title = "Edit Checklist"
+            textField.text = checklist.name
+            doneBarButton.enabled = true
+        }
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        textField.becomeFirstResponder()
+    }
+    
+    
+}
